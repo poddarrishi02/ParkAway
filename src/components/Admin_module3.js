40b1c3f7837438ee33c91fa306a13b33b9ac34e3
poddarrishi02 <poddarrishi02@gmail.com>
2021-11-25 22:55:38 +0530

    Added module 3

---
+++ src/components/Admin_module3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Admin_module3() {
+    return (
+        <div>
+            <div className={StyleSheet.outer}>sdhus</div>
+            <div>scvhsdhs</div>
+        </div>
+    )
+}
+
+export default Admin_module3
