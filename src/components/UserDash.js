554d1959a4d2afd2fbf78cff6aa0a7b15461cd44
poddarrishi02 <poddarrishi02@gmail.com>
2021-12-03 12:12:05 +0530

    dashboard changes

--- src/components/UserDash.js
+++ src/components/UserDash.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../styles/dashboard/admin.module.css'
+import Button from '@mui/material/Button';
+import AddWorker from './AddWorker';
+import Default from './Default';
 
-export default function UserDash() {
+function UserDash() {
+    const [active, setactive] = useState(<AddWorker />);
     return (
-        <div>
-            User
+        <div className={styles.outer}>
+            <div className={styles.topNav}>
+                <div className={styles.navHead}>Admin</div>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.leftNav}>
+                    <div className={styles.dashItems}>
+                    <Button onClick={() => {
+                                        setactive(<AddWorker />)
+                                    }} variant="text" style={{ fontWeight:"bolder",fontFamily:"Ubuntu",minWidth:"100%",justifyContent: "flex-start", color: "white", textTransform: "none",fontSize: "1.1em"  }}
+                                        sx={{ ':hover': { bgcolor: 'rgb(60,60,60)' } }}>View Profile</Button>
+                    </div>
+                    <div className={styles.dashItems}>
+                    <Button onClick={() => {
+                                        setactive(<AddWorker />)
+                                    }} variant="text" style={{ fontWeight:"bolder",fontFamily:"Ubuntu",minWidth:"100%",justifyContent: "flex-start", color: "white", textTransform: "none",fontSize: "1.1em"  }}
+                                        sx={{ ':hover': { bgcolor: 'rgb(60,60,60)' } }}>Book Slots</Button>
+                    </div>
+                </div>
+                <div className={styles.display}>
+                    {active}
+                </div>
+            </div>
         </div>
     )
 }
+
+export default UserDash
+
